a8fb0307483cf89ef8337a09c277597287296a92
hanzhengshen <945961510@qq.com>
2013-08-13 23:16:08 +0800

    我

---
+++ WowYi/src/com/balsampearshz/wowyi/bean/Novel.java
@@ -0,0 +1,173 @@
+package com.balsampearshz.wowyi.bean;
+
+import java.io.Serializable;
+
+public class Novel implements Serializable {
+
+	/**
+	 * serialized ID
+	 */
+	private static final long serialVersionUID = -5134464063424772068L;
+	
+	private String n_id;
+	
+	private String n_author;
+	
+	private String n_title;
+	
+	private String n_desc;
+	
+	private String n_addTime;
+	
+	private String n_seq;
+	
+	private String n_status;
+	
+	private String n_download_url;
+	
+	private String n_logo;
+	
+	private String n_download_count;
+
+	/**
+	 * @return the n_id
+	 */
+	public String getN_id() {
+		return n_id;
+	}
+
+	/**
+	 * @param n_id the n_id to set
+	 */
+	public void setN_id(String n_id) {
+		this.n_id = n_id;
+	}
+
+	/**
+	 * @return the n_author
+	 */
+	public String getN_author() {
+		return n_author;
+	}
+
+	/**
+	 * @param n_author the n_author to set
+	 */
+	public void setN_author(String n_author) {
+		this.n_author = n_author;
+	}
+
+	/**
+	 * @return the n_title
+	 */
+	public String getN_title() {
+		return n_title;
+	}
+
+	/**
+	 * @param n_title the n_title to set
+	 */
+	public void setN_title(String n_title) {
+		this.n_title = n_title;
+	}
+
+	/**
+	 * @return the n_desc
+	 */
+	public String getN_desc() {
+		return n_desc;
+	}
+
+	/**
+	 * @param n_desc the n_desc to set
+	 */
+	public void setN_desc(String n_desc) {
+		this.n_desc = n_desc;
+	}
+
+	/**
+	 * @return the n_addTime
+	 */
+	public String getN_addTime() {
+		return n_addTime;
+	}
+
+	/**
+	 * @param n_addTime the n_addTime to set
+	 */
+	public void setN_addTime(String n_addTime) {
+		this.n_addTime = n_addTime;
+	}
+
+	/**
+	 * @return the n_seq
+	 */
+	public String getN_seq() {
+		return n_seq;
+	}
+
+	/**
+	 * @param n_seq the n_seq to set
+	 */
+	public void setN_seq(String n_seq) {
+		this.n_seq = n_seq;
+	}
+
+	/**
+	 * @return the n_status
+	 */
+	public String getN_status() {
+		return n_status;
+	}
+
+	/**
+	 * @param n_status the n_status to set
+	 */
+	public void setN_status(String n_status) {
+		this.n_status = n_status;
+	}
+
+	/**
+	 * @return the n_download_url
+	 */
+	public String getN_download_url() {
+		return n_download_url;
+	}
+
+	/**
+	 * @param n_download_url the n_download_url to set
+	 */
+	public void setN_download_url(String n_download_url) {
+		this.n_download_url = n_download_url;
+	}
+
+	/**
+	 * @return the n_logo
+	 */
+	public String getN_logo() {
+		return n_logo;
+	}
+
+	/**
+	 * @param n_logo the n_logo to set
+	 */
+	public void setN_logo(String n_logo) {
+		this.n_logo = n_logo;
+	}
+
+	/**
+	 * @return the n_download_count
+	 */
+	public String getN_download_count() {
+		return n_download_count;
+	}
+
+	/**
+	 * @param n_download_count the n_download_count to set
+	 */
+	public void setN_download_count(String n_download_count) {
+		this.n_download_count = n_download_count;
+	}
+	
+	
+}
